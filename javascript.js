2d024db56e9a19d59dd86b746066dde1cc136283
Julia Ricketti <julsricketti@gmail.com>
2015-02-06 16:57:41 -0500

    Skeleton for the JavaScript buttons and dialog created

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
+//this was just a test method
+/*$(document).ready(function(){
   $('button').fadeOut('fast');
-});
+});*/
+
+function newButton(){
+	alert("new button has been clicked");
+}
+function clearButton(){
+	alert("clear button has been clicked");
+}
+
+var dimentions = prompt("What size would you like the board to be?",'5');
+if(dimentions==null){
+	dimentions=5; //default value for now is 5
+}
+alert("You have chosen: "+dimentions);
