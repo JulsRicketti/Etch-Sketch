cbfbd75085616bce883f8d93aa02793d39949626
Julia Ricketti <julsricketti@gmail.com>
2015-07-17 12:01:45 -0400

    Changed the design/style and eliminated the initial questions

--- javascript.js
+++ javascript.js
@@ -10,8 +10,16 @@ function clearButton(){
 	alert("clear button has been clicked");
 }
 
-var dimentions = prompt("What size would you like the board to be?",'5');
-if(dimentions==null){
-	dimentions=5; //default value for now is 5
+function createTable(dimensions){
+	rows = dimensions;
+	columns = dimensions;
+	//figure out where to go from here by looking up dynamic tables on javascript or jquery
+
 }
-alert("You have chosen: "+dimentions);
+
+//not going to chose the dimension manually anymore! So just keep this for later
+/*var dimensions = prompt("What size would you like the board to be?",'5');
+if(dimensions==null){
+	dimensions=5; //default value for now is 5
+}
+alert("You have chosen: "+dimensions);*/
