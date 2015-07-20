a5f15495d0996a4f67955c2d815ce6432e400b02
Julia Ricketti <julsricketti@gmail.com>
2015-07-20 12:54:26 -0400

    implemented the Clear button function

--- javascript.js
+++ javascript.js
@@ -2,26 +2,33 @@
 /*$(document).ready(function(){
   $('button').fadeOut('fast');
 });*/
+var dimensions;
+
+$(document).ready(function(){
+	dimensions=10;
+	createTable(dimensions);
+});
 
 function newButton(){
 	alert("new button has been clicked");
 }
 function clearButton(){
-	alert("clear button has been clicked");
+	$('#etch_sketch_table').remove();
+	createTable(dimensions);
 }
 
 function createTable(dimensions){
-	var content = "<table align='center'>"
+	var content = "<div id='etch_sketch_table'><table align='center'>"
 	
 	for(var i=0; i<dimensions; i++){
 		content+='<tr>';
 		for (var j=0; j<dimensions; j++){
-	    content += '<td class ="etch-sketch-table"></td>';			
+	    content += '<td class ="etch-sketch-item"></td>';			
 		}
 
 	    content+='</tr>';
 	}
-	content += "</table>"
-	$('#etch_sketch_table').append(content);
+	content += "</table>  </div>"
+	$('#table-place-holder').append(content);
 }
 
